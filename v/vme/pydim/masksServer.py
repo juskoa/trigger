3818e3125c05473b10301a8031dd8174f11cd094
Anton Jusko <aj@tp.(none)>
2012-06-23 23:10:03 +0200

    pydim/masksServer.py: oki: on kill -s SIGUSR1 updates all CTPBCM/... services

--- v/vme/pydim/masksServer.py
+++ v/vme/pydim/masksServer.py
@@ -2,7 +2,7 @@
 # 16.5.2012 services:
 # CTPBCM/X string
 # where X can be (like in VALID.BCMASKS): EMPTY B AC A C ACE S SA SC D ?
-import sys,os,time,string,signal,pydim, pylog
+import sys,os,time,string,signal,pydim, pylog, trigdb
 
 def rmzero(strg):
   if strg[-1]=='\0':
@@ -18,26 +18,41 @@ def loctime(epoch_str):
   lt= time.localtime(float(epoch_str))
   rc="%2.2d.%2.2d %2.2d:%2.2d"%(lt[2], lt[1], lt[3], lt[4])
   return rc   # dd.mm hh:mm
+
+services=[]
+
 def signal_handler(signal, stack):
   global quit
   mylog.logm("signal:%d received (1:SIGHUP, 10:SIGUSR1)."%signal)
   if signal==1:
     quit= 'q'
+  if signal==10:
+    bcmasks= trigdb.TrgMasks()
+    for serv in services:
+      value= bcmasks.getmask(serv.bcmname())
+      print "msk:",serv.bcmname(), value,":"
+      serv.update(value)
 
 class service:
   def __init__(self, name, tag):
     self.name= name
+    servicename= "CTPBCM/" + self.name
     self.tag= tag
     mylog.logm("Adding service "+self.name)
-    self.sid = pydim.dis_add_service(self.name, "C", scope_cb, tag)
+    self.sid = pydim.dis_add_service(servicename, "C", scope_cb, tag)
     # A service must be updated before using it.
-    pydim.dis_update_service(self.sid)
+    pydim.dis_update_service(self.sid,(" "),)
     pass
   def update(self, value=None):
     if value==None:
       pydim.dis_update_service(self.sid)
     else:
       pydim.dis_update_service(self.sid,("%s"%(value),))
+  def bcmname(self):
+    if self.name=='E':
+      return "bcmEMPTY"
+    else:
+      return "bcm"+self.name
 
 def scope_cb(tag):
     """
@@ -52,11 +67,12 @@ def scope_cb(tag):
     now = time.strftime("%X")
     # Remember, the callback function must return a tuple
     return ("%s. %s"%(now,"blabla"),)
+    #return ("",)
 #def updateAll():
 #  for line in 
 
 def main():
-  global mylog
+  global mylog, services
   sids= {}
   mylog= pylog.Pylog(None,"ttyYES")   # only tty (no file log)
   dnsnode= pydim.dis_get_dns_node()
@@ -67,16 +83,14 @@ def main():
   mylog.logm("dns:"+dnsnode+ " mypid:"+mypid)
   signal.signal(signal.SIGUSR1, signal_handler)  # 10         SIGUSR1
   signal.signal(signal.SIGHUP, signal_handler)   # 1  kill -s SIGHUP mypid
-  services=[]
-  mt= 'B' ; mtag=1
-  servicename= "CTPBCM/" + mt
-  #sids[mt] = pydim.dis_add_service(servicename, "C", scope_cb, mtag)
-  services.append(service(servicename, mtag))
+  mtall= ['B','A','C','S','SA','SC','D','E']
+  for mtag in range(len(mtall)):
+    services.append(service(mtall[mtag], mtag))
   pydim.dis_start_serving("example of simple server")
   mylog.logm("Starting the server ...")
   while True:
     try:
-      a= raw_input('enter 1 2 or q:\n')
+      a= raw_input('enter 1 2 3: update from VALID.BCMASKS or q:\n')
     except:
       mylog.logm("exception:"+str(sys.exc_info()[0]))
       if quit=='q':
@@ -95,6 +109,12 @@ def main():
       mylog.logm(msg)
       #pydim.dis_update_service(sids[mt],("%s"%(tist+"\0"),))
       services[0].update(msg)
+    elif a=='3':   # read VALID.BCMASKS and update all service
+      bcmasks= trigdb.TrgMasks()
+      for serv in services:
+        value= bcmasks.getmask(serv.bcmname())
+        print "msk:",serv.bcmname(), value,":"
+        serv.update(value)
     else:
       mylog.logm('bad input:%s...'%a) ; continue
   pydim.dis_stop_serving()
