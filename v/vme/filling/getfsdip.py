21b22d68b8f2ac25e8cbca956ab3e3aef2e8d7fc
Anton Jusko <trigger@alidcscom188.cern.ch>
2012-07-19 17:09:36 +0200

    getfsdip.py bug fixed: now BCmasks not touched if incomplete info from DIP

--- v/vme/filling/getfsdip.py
+++ v/vme/filling/getfsdip.py
@@ -44,8 +44,42 @@ def main(action):
     bcwritten= int(dipout[4])
     mylog.infolog("DIP: %s %d/%d bunches"%(dipout[1], bcdip, bcwritten))
     if bcwritten<1:
-       mylog.infolog("Trying to get filling scheme from DIP: no BCs published", level='i')
+       mylog.infolog("Trying to get filling scheme from DIP: no BCs published", level='w')
        rc=2
+    else:
+      if bcwritten != 2*bcdip:
+        mylog.infolog("Incomplete data from DIP:exp:%d got:%d"%(2*bcdip, bcwritten), level='f')
+        rc=2
+  if rc==0:
+    schname= dipout[1]
+    #cmd= os.path.join(os.environ["VMECFDIR"], "filling/lhc2ctp.py")
+    import lhc2ctp
+    reload(lhc2ctp)
+    #schname="test"
+    #mylog.infolog("preparing colission schedule and CTP masks (%d bcs)..."%bcwritten)
+    lsf= open(schname+".dip"); ee=lsf.read(); lsf.close;
+    alice= lhc2ctp.bu2bcstr(ee, schname, format="from dip")
+    lsf= open(schname+".alice","w"); lsf.write(alice); lsf.close;
+    mask= lhc2ctp.FilScheme("", alice); mask= mask.getMasks()
+    lsf= open(schname+".mask","w"); lsf.write(mask); lsf.flush(); lsf.close;
+    #os.fsync()
+    #
+    if action=="test":
+      mylog.infolog("test: CTPRCFG/CS not updated. CS and masks left in fs_auto/")
+      #shutil.copyfile(schname+".mask", "../VALID.BCMASKSauto")
+      rc=10
+    elif action=="bcmasks":
+      mylog.infolog("CTPRCFG/CS not updated (not required in ACT). CS and masks left in fs_auto/")
+    elif action=="auto":
+      mylog.infolog("updating CTPRCFG/CS DIM service and BCMASK file...")
+      #shutil.copyfile(schname+".mask", "../VALID.BCMASKS")
+      cmd= "cp "+schname+".mask ../VALID.BCMASKS"
+      mylog.infolog("cmd:"+cmd)
+      os.system(cmd)
+      rc= os.WEXITSTATUS(os.system("colschedule.bash update_auto >/dev/null"))
+    else:
+      rc=99
+    mylog.infolog("colschedule.bash rc:%d"%rc)
   if rc==0:
     schname= dipout[1]
     #cmd= os.path.join(os.environ["VMECFDIR"], "filling/lhc2ctp.py")
@@ -89,6 +123,7 @@ getfsdip.py auto
             test
 rc:
  1: cannot get DIP data
+ 2: Incomplete data published over DIP
 10: test i.e. CS and masks not changed.
 99: bad action, this message
 """
