69029e19dca0650c8cd4d62958c3df1798be8d29
juskoa <trigger@alidcscom835.cern.ch>
2018-04-10 16:53:54 +0200

    document files update

--- v/bin/ctpinpsort.sh
+++ v/bin/ctpinpsort.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo see also $VMECFDIR/inputs/inputs.cfg
-echo "sorting l0 inputs by ctp-l0 (1..24) to /tmp/l0s.cfg ..."
-grep -v -e'^#' -e'^1' -e'^l0' $dbctp/ctpinputs.cfg |sort -n -k6,6 >/tmp/l0s.cfg
+if [[ $# -eq 0 ]] ;then
+  echo see also $VMECFDIR/inputs/inputs.cfg
+  ctpins=$dbctp/ctpinputs.cfg
+  echo "sorting l0 inputs by ctp-l0 (1..24) to /tmp/l0s.cfg ..."
+else
+  echo "sorting l0 inputs in $ctpins by ctp-l0 (1..24) to /tmp/l0s.cfg ..."
+  ctpins=$1
+fi
+grep -v -e'^#' -e'^1' -e'^l0' $ctpins |sort -n -k6,6 >/tmp/l0s.cfg
